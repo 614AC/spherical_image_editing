cec1d69c7f0b81042b2da8384b45d5dc628e1cea
Drew <andrew.macquarrie@gmail.com>
2017-04-11 13:28:33 +0100

    readding double image params (but not yet using); cleanup removing lots of unused code; have gotten to state where is clean but differentiating between spheres using recurse value - currently not showing previous spheres but next spheres still there

--- sphere_transforms.py
+++ sphere_transforms.py
@@ -143,29 +143,6 @@ def rotate_pixel_coords_p_to_q(p, q, x_size = 1000):
   q = sphere_from_pixel_coords(q, x_size = x_size)
   return rotate_sphere_points_p_to_q(p,q)
 
-def rotate_around_axis_sphere_points_p_q(p,q,theta):
-  """p and q are points on sphere, return SL(2,C) matrix rotating by angle theta around the axis from p to q"""
-  p, q = vector(p), vector(q)
-  CP1p, CP1q = CP1_from_sphere(p), CP1_from_sphere(q)
-  assert dot(p,q) < 0.9999, "axis points should not be in the same place!"
-  r = get_vector_perp_to_p_and_q(p, q)
-  CP1r = CP1_from_sphere(r)
-  M_standardise = two_triples_to_SL(CP1p, CP1q, CP1r, [0,1], [1,0], [1,1])
-  M_theta = [[complex(cos(theta),sin(theta)),0],[0,1]] #rotate on axis through 0, infty by theta
-  return matrix_mult( matrix_mult(matrix2_inv(M_standardise), M_theta), M_standardise )
-
-def rotate_around_axis_pixel_coords_p_q(p,q,theta, x_size = 1000):
-  """p and q are pixel coordinate points, return SL(2,C) matrix rotating by angle theta around the axis from p to q"""
-  p = sphere_from_pixel_coords(p, x_size = x_size)
-  q = sphere_from_pixel_coords(q, x_size = x_size)
-  return rotate_around_axis_sphere_points_p_q(p,q,theta)
-
-def rotate_around_axis_pixel_coord_p(p,theta, x_size = 1000):
-  """p is a pixel coordinate point, return SL(2,C) matrix rotating by angle theta around the axis from p to its antipode"""
-  p = sphere_from_pixel_coords(p, x_size = x_size)
-  minus_p = -p
-  return rotate_around_axis_sphere_points_p_q(p,minus_p,theta)
-     
 def zoom_in_on_pixel_coords(p, zoom_factor, x_size = 1000):
   """p is pixel coordinate point, return SL(2,C) matrix zooming in on p by a factor of scale"""
   # Note that the zoom factor is only accurate at the point p itself. As we move away from p, we zoom less and less.
@@ -173,29 +150,6 @@ def zoom_in_on_pixel_coords(p, zoom_factor, x_size = 1000):
   M_rot = rotate_pixel_coords_p_to_q( p, (0,0), x_size = x_size)
   M_scl = [[zoom_factor,0],[0,1]] ### zoom in on zero in CP^1
   return matrix_mult( matrix_mult(matrix2_inv(M_rot), M_scl), M_rot )
-
-def zoom_along_axis_sphere_points_p_q(p, q, zoom_factor):
-  """p and q are points on sphere, return SL(2,C) matrix zooming along axis from p to q"""
-  p, q = vector(p), vector(q)
-  CP1p, CP1q = CP1_from_sphere(p), CP1_from_sphere(q)
-  assert dot(p,q) < 0.9999   #points should not be in the same place
-  r = get_vector_perp_to_p_and_q(p, q)
-  CP1r = CP1_from_sphere(r)
-  M_standardise = two_triples_to_SL(CP1p, CP1q, CP1r, [0,1], [1,0], [1,1])
-  M_theta = [[zoom_factor,0],[0,1]] 
-  return matrix_mult( matrix_mult(matrix2_inv(M_standardise), M_theta), M_standardise )
-
-def zoom_along_axis_pixel_coords_p_q(p, q, zoom_factor, x_size = 1000):
-  """p and q are pixel coordinate points, return SL(2,C) matrix zooming along axis from p to q by zoom_factor"""
-  # This function does the same thing as zoom_in_on_pixel_coords, but with the 
-  # two given points instead of a single point and its antipodal point
-  p = sphere_from_pixel_coords(p, x_size = x_size)
-  q = sphere_from_pixel_coords(q, x_size = x_size)
-  return zoom_along_axis_sphere_points_p_q(p,q,zoom_factor)
-
-def translate_along_axis_pixel_coords(p, q, r1, r2, x_size = 1000):
-  """Return SL(2,C) matrix translating/rotating on the axis from p to q, taking r1 to r2"""
-  return three_points_to_three_points_pixel_coords(p,q,r1,p,q,r2, x_size = x_size)
 
 ##### Apply functions to images
 
@@ -225,24 +179,20 @@ def apply_SL2C_elt_to_image(M, source_image_filename, out_x_size = None, save_fi
       o_im[i,j] = get_interpolated_pixel_colour(pt, s_im, x_size = in_x_size)
   out_image.save(save_filename)
 
-def droste_effect(zoom_center_pixel_coords, zoom_factor, zoom_cutoff, source_image_filename, out_x_size = None, twist = False, zoom_loop_value = 0.0, save_filename = "sphere_transforms_test.png"):
+def droste_effect(zoom_center_pixel_coords, zoom_factor, zoom_cutoff, source_image_filename_A, source_image_filename_B, out_x_size = None, zoom_loop_value = 0.0, save_filename = "sphere_transforms_test.png"):
   """produces a zooming Droste effect image from an equirectangular source image"""
-  # The source image should be a composite of the original image together with a zoomed version, 
-  # fit inside a picture frame or similar in the original image
-  source_image = Image.open(source_image_filename)
-  s_im = source_image.load()
-  in_x_size, in_y_size = source_image.size
+  source_image_A = Image.open(source_image_filename_A)
+  s_im_A = source_image_A.load()
+  source_image_B = Image.open(source_image_filename_B)
+  s_im_B = source_image_B.load()
+  
+  in_x_size, in_y_size = source_image_A.size
   
   M_rot = rotate_pixel_coords_p_to_q(zoom_center_pixel_coords, (0,0), x_size = in_x_size)
   M_rot_inv = matrix2_inv(M_rot)
-  if out_x_size == None:
-    out_x_size, out_y_size = source_image.size
-  else:
-    out_y_size = out_x_size/2
+  out_y_size = out_x_size/2
   out_image = Image.new("RGB", (out_x_size, out_y_size))
   o_im = out_image.load()
-
-  droste_factor = ( cmath.log(zoom_factor) + complex(0, 2*pi) ) / complex(0, 2*pi)  # used if twisting 
 
   for i in range(out_x_size): 
     for j in range(out_y_size):
@@ -258,101 +208,52 @@ def droste_effect(zoom_center_pixel_coords, zoom_factor, zoom_cutoff, source_ima
       # make some very specific choices of where to zoom to etc. 
       pt = pt[0]/pt[1]  
       pt = cmath.log(pt)
-      if twist:  # otherwise straight zoom
-        pt = droste_factor * pt  
 
       # zoom_loop_value is between 0 and 1, vary this from 0.0 to 1.0 to animate frames zooming into the droste image
       pt = complex(pt.real + log(zoom_factor) * zoom_loop_value, pt.imag) 
+      recurse_value = (pt.real + zoom_cutoff) // log(zoom_factor)
+      
+      #print recurse_value
       
       # zoom_cutoff alters the slice of the input image that we use, so that it covers mostly the original image, together with 
       # some of the zoomed image that was composited with the original. The slice needs to cover the seam between the two
       # (i.e. the picture frame you are using, but should cover as little as possible of the zoomed version of the image.
-      pt = complex((pt.real + zoom_cutoff) % log(zoom_factor) - zoom_cutoff, pt.imag) 
+      
+      if(int(recurse_value) >= 0): # int() is math floor function. main and prev spheres
+        someval = "nothing happening"
+      else:
+        pt = complex((pt.real + zoom_cutoff) % log(zoom_factor) - zoom_cutoff, pt.imag)
+        # do nothing to pt
+      
+      
       pt = cmath.exp(pt)
       pt = [pt, 1] #back to projective coordinates
       pt = matrix_mult_vector(M_rot_inv, pt)
       pt = sphere_from_CP1(pt)
       pt = angles_from_sphere(pt)
       pt = pixel_coords_from_angles(pt, x_size = in_x_size)
-      o_im[i,j] = get_interpolated_pixel_colour(pt, s_im, in_x_size)
+      o_im[i,j] = get_interpolated_pixel_colour(pt, s_im_A, in_x_size)
+      
+      '''
+      if(int(recurse_value) == -1):
+        o_im[i,j] = (222,0,0)
+      '''
 
+  print save_filename
   out_image.save(save_filename)
 
-def batch_transform_frames(in_dirname, out_dirname, M_func, frame_num_range = None, func_range = None, out_x_size = None):
-  """transform a directory of numbered frame images according to a given function, that outputs a matrix as a function of time"""
-  ### assumes num_frames is a three digit number
-  ### M_func should be a function that takes in a float between 0.0 and 1.0 and returns an SL(2,C) matrix
-  ### frame_num_range is a pair, telling us which frames to process
-  ### func_range is a pair, the first telling us the frame corresponding to t = 0.0, and the second to t = 1.0
-  ### these two ranges could be different if you are running multiple processes simultaneously, 
-  ### dealing with different frame_num_ranges, but running the same function with the same func_range
-  filenames = os.listdir(in_dirname)
-  start, end = frame_num_range
-  fstart, fend = func_range
-  for filename in filenames: 
-    fullname = in_dirname + '/' + filename
-    if not os.path.exists(fullname):
-      print('file not found!')
-    else:
-      if filename[-4:] == '.png':
-        frame_num = int(filename[-7:-4])
-        if start <= frame_num < end:
-          print('frame ', int(filename[-7:-4]))
-          t = float(frame_num - fstart)/float((fend-1) - fstart)  ## 0.0 <= t <= 1.0
-          M = M_func(t)
-          apply_SL2C_elt_to_image( M, fullname, out_x_size = out_x_size, save_filename = out_dirname + '/' + filename )
-  print('done')
-
-######## Useful functions to use when making transformation functions to put into batch_transform_frames
-
-def up_down(t):
-  """ graph: /\ """
-  if t < 0.5:
-    return 2*t
-  else:
-    return 2-2*t
-
-def up_plateau_down(t):
-  """ graph: /"\ """
-  if t < 0.33333:
-    return 3*t
-  elif t < 0.66667:
-    return 1.0
-  else:
-    return 3 - 3*t
 
 ######## Test 
 
 if __name__ == "__main__":
+  #M = zoom_in_on_pixel_coords((360,179.5), 2, x_size = 720) 
+  #apply_SL2C_elt_to_image( M, 'equirectangular_test_image.png', save_filename = 'scaled_test_image.png' )
 
-  ###### Apply transformations to single frames
-
-  M = zoom_in_on_pixel_coords((360,179.5), 2, x_size = 720) 
-  apply_SL2C_elt_to_image( M, 'equirectangular_test_image.png', save_filename = 'scaled_test_image.png' )
-
-  # M = rotate_around_axis_pixel_coord_p((360,179.5), pi/8, x_size = 720)
-  # apply_SL2C_elt_to_image( M, 'equirectangular_test_image.png', save_filename = 'rotated_test_image.png' )
-
-  ###### Apply transformations to a directory of frames
-
-  ### Assume you have a directory called "foo_in" that has 1920x960 png frame images called something like 
-  ### "frame_000.png", "frame_001.png", ... "frame_099.png", and a directory "foo_out" you want to save 
-  ### images to. Then, first define the transformation function (as a function of time):
-
-  # def my_zoom(t):
-    # return zoom_in_on_pixel_coords((960,480), 1 + up_plateau_down(t), x_size = 1920) 
-
-  ### Then run the batch function
-
-  # batch_transform_frames("foo_in", "foo_out", my_zoom, frame_num_range = (0, 100), func_range = (0, 100), out_x_size = 1920)
-
-  ###### Make droste animations
-
-  # num_frames = 4
-  # for i in range(num_frames):
-  #   zoom_loop_value = float(i)/float(num_frames)
-  #   droste_effect((2650,1300), 7.3, 1.0, '(elevr+h)x2_one_zoom_7.3.png', out_x_size = 1920, twist = False, zoom_loop_value = zoom_loop_value, save_filename = "droste_straight_anim_frames/droste_anim_straight_" + str(i).zfill(3) + ".png")
-  #   droste_effect((2650,1300), 7.3, 1.0, '(elevr+h)x2_one_zoom_7.3.png', out_x_size = 1920, twist = True, zoom_loop_value = zoom_loop_value, save_filename = "droste_twist_anim_frames/droste_anim_twist_" + str(i).zfill(3) + ".png")
+  num_frames = 4
+  for i in range(num_frames):
+    zoom_loop_value = float(i)/float(num_frames)
+    droste_effect((2650,1300), 7.3, 1.0, '(elevr+h)x2_one_zoom_7.3.png', 'equirect_square.jpg', out_x_size = 600, zoom_loop_value = zoom_loop_value, save_filename = "droste_straight_anim_frames/droste_anim_straight_" + str(i).zfill(3) + ".png")
+  #  droste_effect((2650,1300), 7.3, 1.0, '(elevr+h)x2_one_zoom_7.3.png', out_x_size = 1920, twist = True, zoom_loop_value = zoom_loop_value, save_filename = "droste_twist_anim_frames/droste_anim_twist_" + str(i).zfill(3) + ".png")
 
 
 
