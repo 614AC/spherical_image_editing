7752dc44a64acba5706e958b9bf7d405802fe450
Drew <andrew.macquarrie@gmail.com>
2017-04-23 14:49:26 +0100

    basics working

--- combined.py
+++ combined.py
@@ -1,11 +1,14 @@
 import os
 from bubble_animation import bubble_anim
 from sphere_transforms import droste_effect, rotate_equirect_image
+from datetime import datetime
+import uuid
 
 
 def one_shot(output_folder, pano_A_filename, pano_B_filename, pano_A_zoom_centre, pano_B_zoom_centre, bubble_in_diameter, bubble_out_diameter, zoom_factor, zoom_cutoff, subtract_amount):
-  num_frames = 10
-  image_width = 400 # 4096
+  num_frames = 4
+  image_width = 4096
+  image_height = image_width / 2
   
   # create the output folder
   if not os.path.exists(output_folder):
@@ -18,10 +21,10 @@ def one_shot(output_folder, pano_A_filename, pano_B_filename, pano_A_zoom_centre
   
   # generate the droste
   print "start: " + datetime.now().strftime('%H:%M:%S')
-  for i in rangeWithEnd(0, num_frames + 1):
+  for i in range(0, num_frames + 1):
     zoom_loop_value = -1 * float(i) / float(num_frames)
     # TODO: send subtract amount
-    droste_effect(zoom_point_A, zoom_factor, zoom_cutoff, pano_A_filename, rotated_pano_B_filename, out_x_size = image_width, zoom_loop_value = zoom_loop_value, save_filename = output_folder + "/droste_anim_" + str(i).zfill(3) + ".png")
+    droste_effect(pano_A_zoom_centre, zoom_factor, zoom_cutoff, pano_A_filename, rotated_pano_B_filename, out_x_size = image_width, zoom_loop_value = zoom_loop_value, save_filename = output_folder + "/droste_anim_" + str(i).zfill(3) + ".png")
   
   # generate the bubble in anim
   pano_A_no_bubble = pano_A_filename
@@ -35,7 +38,8 @@ def one_shot(output_folder, pano_A_filename, pano_B_filename, pano_A_zoom_centre
   pano_B_no_bubble = rotated_pano_B_filename
   pano_B_bubble = output_folder + "/droste_anim_" + str(num_frames).zfill(3) + ".png"
   bubble_B_diam = 1808.0
-  bubble_B_position = (((image_width / 2) + pano_A_zoom_centre) % image_width, image_height / 2)
+  bubble_B_position = (((image_width / 2) + pano_A_zoom_centre[0]) % image_width, image_height / 2)
+  print bubble_B_position
 
   for i in range(int(num_frames),1,-1):
     bubble_anim(pano_B_bubble,pano_B_no_bubble, bubble_B_position, bubble_B_diam, float(i)/num_frames, output_folder + "/out_bubble_anim_" + str(int(num_frames - i)).zfill(3) + ".png")
@@ -49,8 +53,8 @@ if __name__ == '__main__':
   pano_A_zoom_centre = (1464,1024)
   pano_B_zoom_centre = (3485,1024)
 
-  bubble_out_diam = 1808.0
-  bubble_in_diam = 760.0
+  bubble_out_diameter = 1808.0
+  bubble_in_diameter = 760.0
   
   pano_A_filename = './buddha/37e2e38392994f83b67c96a6c9e71e1f_pano.jpg'
   pano_B_filename = './buddha/7c79262fda81415fa384036bd04b30b3_pano.jpg'
