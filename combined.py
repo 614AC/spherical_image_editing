e8a1fc3d749a6081bbfba65e78788b2417bd3483
derican <gradymelman@hotmail.com>
2021-12-14 23:57:22 +0800

    batch

--- combined.py
+++ combined.py
@@ -8,6 +8,7 @@ import imageio
 from threading import Thread
 from tqdm import trange
 from multiprocessing import Pool
+from config import mapping_dict
 
 
 def loop_generate_image(s, t, zoom_center_pixel_coords, zoom_factor,
@@ -74,13 +75,10 @@ def generate(output_folder,
         args = list()
         for i in range(num_frames + 1):
             zoom_loop_value = -1 * float(i) / float(num_frames)
-            args.append((pano_A_zoom_centre,
-                         zoom_factor,
-                         zoom_cutoff,
-                         pano_A_filename,
-                         rotated_pano_B_filename,
-                         zoom_loop_value,
-                         output_folder + "\\droste_anim_" + str(i).zfill(3) + ".png"))
+            args.append(
+                (pano_A_zoom_centre, zoom_factor, zoom_cutoff, pano_A_filename,
+                 rotated_pano_B_filename, zoom_loop_value,
+                 output_folder + "\\droste_anim_" + str(i).zfill(3) + ".png"))
 
         with Pool(processes=4) as pool:
             pool.starmap(generate_image, args)
@@ -91,10 +89,10 @@ def generate(output_folder,
     bubble_A_diam = 760.0
 
     for i in trange(1, bubble_num_frames):
-        bubble_anim(pano_A_bubble, pano_A_no_bubble,
-                    pano_A_zoom_centre, bubble_A_diam, float(
-                        i)/float(bubble_num_frames),
-                    output_folder + "\\in_bubble_anim_"+str(i).zfill(3) + ".png")
+        bubble_anim(
+            pano_A_bubble, pano_A_no_bubble, pano_A_zoom_centre, bubble_A_diam,
+            float(i) / float(bubble_num_frames),
+            output_folder + "\\in_bubble_anim_" + str(i).zfill(3) + ".png")
 
     # generate the bubble out anim
     pano_B_no_bubble = rotated_pano_B_filename
@@ -103,13 +101,14 @@ def generate(output_folder,
     bubble_B_diam = 760.0
     bubble_B_position = (int(
         ((image_width / 2) + pano_A_zoom_centre[0]) % image_width),
-        int(image_height / 2))
+                         int(image_height / 2))
 
     for i in trange(bubble_num_frames, 1, -1):
-        bubble_anim(pano_B_bubble,
-                    pano_B_no_bubble, bubble_B_position, bubble_B_diam,
-                    float(i)/bubble_num_frames,
-                    output_folder + "\\out_bubble_anim_"+str(int(bubble_num_frames - i)).zfill(3) + ".png")
+        bubble_anim(
+            pano_B_bubble, pano_B_no_bubble, bubble_B_position, bubble_B_diam,
+            float(i) / bubble_num_frames,
+            output_folder + "\\out_bubble_anim_" +
+            str(int(bubble_num_frames - i)).zfill(3) + ".png")
 
 
 def start_one_shot_generation(pano_A_zoom_centre, pano_B_zoom_centre,
@@ -135,9 +134,17 @@ def start_one_shot_generation(pano_A_zoom_centre, pano_B_zoom_centre,
     num_frames = 120
     bubble_num_frames = 30
 
-    generate(folder_name, pano_A_filepath, pano_B_filepath, pano_A_zoom_centre,
-             pano_B_zoom_centre, bubble_in_diameter, bubble_out_diameter,
-             zoom_factor, zoom_cutoff, num_frames=num_frames, bubble_num_frames=bubble_num_frames)
+    generate(folder_name,
+             pano_A_filepath,
+             pano_B_filepath,
+             pano_A_zoom_centre,
+             pano_B_zoom_centre,
+             bubble_in_diameter,
+             bubble_out_diameter,
+             zoom_factor,
+             zoom_cutoff,
+             num_frames=num_frames,
+             bubble_num_frames=bubble_num_frames)
 
     imgs = []
     for i in range(1, bubble_num_frames):
@@ -152,16 +159,28 @@ def start_one_shot_generation(pano_A_zoom_centre, pano_B_zoom_centre,
         imgs.append(
             imageio.imread(folder_name + "\\out_bubble_anim_" +
                            str(i).zfill(3) + ".png"))
-    imageio.mimsave(".\\video\\" + pano_a_short_name.split('_')[0] +
-                    "_" + pano_b_short_name.split('_')[0] + ".mp4",
-                    imgs, fps=30)
+    imageio.mimsave(".\\video\\" + pano_a_short_name.split('_')[0] + "_" +
+                    pano_b_short_name.split('_')[0] + ".mp4",
+                    imgs,
+                    fps=30)
 
 
 if __name__ == '__main__':
-    pano_A_zoom_xcord = (1610, 535)
-    pano_B_zoom_xcord = (787, 535)
-    node_A_id = '00ebbf3782c64d74aaf7dd39cd561175_skybox'
-    node_B_id = 'ee59d6b5e5da4def9fe85a8ba94ecf25_skybox'
     pano_folder = '.\\matterport_pano_images\\'
-    start_one_shot_generation(pano_A_zoom_xcord, pano_B_zoom_xcord,
-                              pano_folder, node_A_id, node_B_id)
+    defualt_height = 535
+
+    for key in mapping_dict.keys():
+        node_A_id = key
+        children = mapping_dict[key]
+        for keyy in children.keys():
+            node_B_id = keyy
+            pano_A_zoom_xcord = (children[keyy][0], defualt_height)
+            pano_B_zoom_xcord = (children[keyy][1], defualt_height)
+
+            print("Error generating " + node_A_id + " to " + node_B_id)
+
+            try:
+                start_one_shot_generation(pano_A_zoom_xcord, pano_B_zoom_xcord,
+                                          pano_folder, node_A_id, node_B_id)
+            except Exception:
+                print("Error generating " + node_A_id + " to " + node_B_id)
---
+++ config.py
@@ -0,0 +1,52 @@
+mapping_dict = {
+    "00ebbf3782c64d74aaf7dd39cd561175_skybox": {
+        "ee59d6b5e5da4def9fe85a8ba94ecf25_skybox": (1610, 787)
+    },
+    "ee59d6b5e5da4def9fe85a8ba94ecf25_skybox": {
+        "00ebbf3782c64d74aaf7dd39cd561175_skybox": (1839, 589),
+        "7ad0b4e15c4f4cf38f56efbbcadfbf6a_skybox": (1347, 1727),
+        "da5fa65c13e643719a20cbb818c9a85d_skybox": (795, 1489)
+    },
+    "7ad0b4e15c4f4cf38f56efbbcadfbf6a_skybox": {
+        "ee59d6b5e5da4def9fe85a8ba94ecf25_skybox": (675, 185),
+        "85c23efeaecd4d43a7dcd5b90137179e_skybox": (1767, 665)
+    },
+    "85c23efeaecd4d43a7dcd5b90137179e_skybox": {
+        "7ad0b4e15c4f4cf38f56efbbcadfbf6a_skybox": (1799, 675)
+    },
+    "da5fa65c13e643719a20cbb818c9a85d_skybox": {
+        "ee59d6b5e5da4def9fe85a8ba94ecf25_skybox": (551, 1833),
+        "0e92a69a50414253a23043758f111cec_skybox": (1499, 505),
+        "1a41339ece1846eda6a924cdb4c417dd_skybox": (2025, 831)
+    },
+    "0e92a69a50414253a23043758f111cec_skybox": {
+        "da5fa65c13e643719a20cbb818c9a85d_skybox": (1545, 551)
+    },
+    "1a41339ece1846eda6a924cdb4c417dd_skybox": {
+        "da5fa65c13e643719a20cbb818c9a85d_skybox": (1747, 1003),
+        "08c774f20c984008882da2b8547850eb_skybox": (319, 545)
+    },
+    "08c774f20c984008882da2b8547850eb_skybox": {
+        "1a41339ece1846eda6a924cdb4c417dd_skybox": (1641, 1413),
+        "0f37bd0737e349de9d536263a4bdd60d_skybox": (535, 1797)
+    },
+    "0f37bd0737e349de9d536263a4bdd60d_skybox": {
+        "08c774f20c984008882da2b8547850eb_skybox": (803, 1603),
+        "23ec8bbec8eb40478411cba2e55b732e_skybox": (1791, 121)
+    },
+    "23ec8bbec8eb40478411cba2e55b732e_skybox": {
+        "0f37bd0737e349de9d536263a4bdd60d_skybox": (1235, 807),
+        "5b9b2794954e4694a45fc424a8643081_skybox": (719, 1427),
+        "1e86968849944444b66d9537efb5da9e_skybox": (1425, 725)
+    },
+    "5b9b2794954e4694a45fc424a8643081_skybox": {
+        "23ec8bbec8eb40478411cba2e55b732e_skybox": (1429, 27)
+    },
+    "1e86968849944444b66d9537efb5da9e_skybox": {
+        "23ec8bbec8eb40478411cba2e55b732e_skybox": (1825, 977),
+        "8ccf449f599a4cd88648e1d0ff39fdd0_skybox": (511, 523)
+    },
+    "8ccf449f599a4cd88648e1d0ff39fdd0_skybox": {
+        "1e86968849944444b66d9537efb5da9e_skybox": (1573, 1561)
+    }
+}
