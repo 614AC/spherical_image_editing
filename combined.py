f6950d3bc516c26f5a66e3841886971b913f9985
Sandglass-vic <1294958142@qq.com>
2021-12-14 13:11:04 +0800

    use 4 processes to reach a 2x speed up

--- combined.py
+++ combined.py
@@ -67,101 +67,63 @@ def one_shot(output_folder,
 
     # generate the droste
     print("start: " + datetime.now().strftime('%H:%M:%S'))
-    NUM_THREADS = 4
-    threads = []
-    loop_per_threads = int((num_frames + 1) / NUM_THREADS)
-    for i in range(NUM_THREADS):
-        # zoom_loop_value = -1 * float(i) / float(num_frames)
-        # TODO: send subtract amount
-        threads.append(
-            Thread(target=loop_generate_image,
-                   args=(i * loop_per_threads, (i + 1) * loop_per_threads if
-                         (i + 1) * loop_per_threads <= num_frames + 1 else
-                         num_frames + 1, pano_A_zoom_centre, zoom_factor,
-                         zoom_cutoff, pano_A_filename, rotated_pano_B_filename,
-                         image_width, num_frames,
-                         output_folder + "\\droste_anim_")))
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
-        # generate_image(pano_A_zoom_centre,
-        #                zoom_factor,
-        #                zoom_cutoff,
-        #                pano_A_filename,
-        #                rotated_pano_B_filename,
-        #                out_x_size=image_width,
-        #                zoom_loop_value=zoom_loop_value,
-        #                save_filename=output_folder + "\\droste_anim_" +
-        #                str(i).zfill(3) + ".png")
+    for i in trange(num_frames):
+        zoom_loop_value = -1 * float(i) / float(num_frames)
+        generate_image(pano_A_zoom_centre,
+                       zoom_factor,
+                       zoom_cutoff,
+                       pano_A_filename,
+                       rotated_pano_B_filename,
+                       out_x_size=image_width,
+                       zoom_loop_value=zoom_loop_value,
+                       save_filename=output_folder + "\\droste_anim_" +
+                       str(i).zfill(3) + ".png")
 
-    bubble_num_frames = 30
+    # bubble_num_frames = 30
 
-    # generate the bubble in anim
-    pano_A_no_bubble = pano_A_filename
-    pano_A_bubble = output_folder + "\\droste_anim_" + str(0).zfill(3) + ".png"
-    bubble_A_diam = 760.0
+    # # generate the bubble in anim
+    # pano_A_no_bubble = pano_A_filename
+    # pano_A_bubble = output_folder + "\\droste_anim_" + str(0).zfill(3) + ".png"
+    # bubble_A_diam = 760.0
 
-    threads.clear()
-    loop_per_threads = int((bubble_num_frames - 1) / NUM_THREADS)
-    for i in range(NUM_THREADS):
-        threads.append(
-            Thread(target=loop_bubble_in_anim,
-                   args=(1 + i * loop_per_threads,
-                         1 + (i + 1) * loop_per_threads if 1 +
-                         (i + 1) * loop_per_threads <= bubble_num_frames else
-                         bubble_num_frames, pano_A_bubble, pano_A_no_bubble,
-                         pano_A_zoom_centre, bubble_A_diam, bubble_num_frames,
-                         output_folder + "\\in_bubble_anim_")))
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
+    # for i in trange(1, bubble_num_frames):
+    #     bubble_anim(pano_A_bubble, pano_A_no_bubble,
+    #                 pano_A_zoom_centre, bubble_A_diam, float(
+    #                     i)/float(bubble_num_frames),
+    #                 output_folder + "\\in_bubble_anim_"+str(i).zfill(3) + ".png")
 
-    # generate the bubble out anim
-    pano_B_no_bubble = rotated_pano_B_filename
-    pano_B_bubble = output_folder + "\\droste_anim_" + str(
-        bubble_num_frames).zfill(3) + ".png"
-    bubble_B_diam = 760.0
-    bubble_B_position = (int(
-        ((image_width / 2) + pano_A_zoom_centre[0]) % image_width),
-                         int(image_height / 2))
-    print(bubble_B_position)
+    # # generate the bubble out anim
+    # pano_B_no_bubble = rotated_pano_B_filename
+    # pano_B_bubble = output_folder + "\\droste_anim_" + str(
+    #     bubble_num_frames).zfill(3) + ".png"
+    # bubble_B_diam = 760.0
+    # bubble_B_position = (int(
+    #     ((image_width / 2) + pano_A_zoom_centre[0]) % image_width),
+    #     int(image_height / 2))
 
-    threads.clear()
-    loop_per_threads = int((bubble_num_frames - 1) / NUM_THREADS)
-    for i in range(NUM_THREADS):
-        threads.append(
-            Thread(target=loop_bubble_out_anim,
-                   args=(bubble_num_frames - i * loop_per_threads,
-                         bubble_num_frames -
-                         (i + 1) * loop_per_threads if bubble_num_frames -
-                         (i + 1) * loop_per_threads >= 1 else 1, pano_B_bubble,
-                         pano_B_no_bubble, bubble_B_position, bubble_B_diam,
-                         bubble_num_frames,
-                         output_folder + "\\out_bubble_anim_")))
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
+    # for i in trange(bubble_num_frames, 1, -1):
+    #     bubble_anim(pano_B_bubble,
+    #                 pano_B_no_bubble, bubble_B_position, bubble_B_diam,
+    #                 float(i)/bubble_num_frames,
+    #                 output_folder + "\\out_bubble_anim_"+str(int(bubble_num_frames - i)).zfill(3) + ".png")
 
-    imgs = []
-    for i in range(1, bubble_num_frames):
-        imgs.append(
-            imageio.imread(output_folder + "\\in_bubble_anim_" +
-                           str(i).zfill(3) + ".png"))
-    for i in range(num_frames + 1):
-        imgs.append(
-            imageio.imread(output_folder + "\\droste_anim_" + str(i).zfill(3) +
-                           ".png"))
-    for i in range(bubble_num_frames - 1):
-        imgs.append(
-            imageio.imread(output_folder + "\\out_bubble_anim_" +
-                           str(i).zfill(3) + ".png"))
-    imageio.mimsave(output_folder + "\\" + pano_A_filename.split('_')[0] +
-                    "_" + pano_B_filename.split('_')[0] + ".mp4",
-                    imgs,
-                    fps=30)
+    # imgs = []
+    # for i in range(1, bubble_num_frames):
+    #     imgs.append(
+    #         imageio.imread(output_folder + "\\in_bubble_anim_" +
+    #                        str(i).zfill(3) + ".png"))
+    # for i in range(num_frames + 1):
+    #     imgs.append(
+    #         imageio.imread(output_folder + "\\droste_anim_" + str(i).zfill(3) +
+    #                        ".png"))
+    # for i in range(bubble_num_frames - 1):
+    #     imgs.append(
+    #         imageio.imread(output_folder + "\\out_bubble_anim_" +
+    #                        str(i).zfill(3) + ".png"))
+    # imageio.mimsave(output_folder + "\\" + pano_A_filename.split('_')[0] +
+    #                 "_" + pano_B_filename.split('_')[0] + ".mp4",
+    #                 imgs,
+    #                 fps=30)
 
 
 def start_one_shot_generation(pano_A_zoom_centre, pano_B_zoom_centre,
@@ -181,7 +143,8 @@ def start_one_shot_generation(pano_A_zoom_centre, pano_B_zoom_centre,
     # required
     pano_A_filepath = pano_folder + pano_a_filename
     pano_B_filepath = pano_folder + pano_b_filename
-    folder_name = pano_folder + 'test_one_shot_generation\\' + pano_a_short_name + "_to_" + pano_b_short_name
+    folder_name = '.\\test_one_shot_generation\\' + \
+        pano_a_short_name + "_to_" + pano_b_short_name
 
     # try:
     one_shot(folder_name, pano_A_filepath, pano_B_filepath, pano_A_zoom_centre,
--- sphere_transforms.py
+++ sphere_transforms.py
@@ -1,15 +1,18 @@
-#### Python code for performing Mobius transformations to equirectangular images, by Henry Segerman, Jan 2016
+# Python code for performing Mobius transformations to equirectangular images, by Henry Segerman, Jan 2016
 
-#### This code is provided as is, with no warranty or expectation of correctness. Do not use for mission critical
-#### purposes without thoroughly checking it yourself.
+# This code is provided as is, with no warranty or expectation of correctness. Do not use for mission critical
+# purposes without thoroughly checking it yourself.
 
-#### This code uses the Python Imaging Library (PIL), currently supported as "Pillow", available from https://python-pillow.github.io
+# This code uses the Python Imaging Library (PIL), currently supported as "Pillow", available from https://python-pillow.github.io
 
-#### For more details, see the blogpost at http://elevr.com/spherical-video-editing-effects-with-mobius-transformations/
-#### and the tech demo spherical video at https://www.youtube.com/watch?v=oVwmF_vrZh0
+# For more details, see the blogpost at http://elevr.com/spherical-video-editing-effects-with-mobius-transformations/
+# and the tech demo spherical video at https://www.youtube.com/watch?v=oVwmF_vrZh0
 
+import multiprocessing
 import os
 from math import *
+
+from numpy.core.fromnumeric import shape
 from vectors_and_matrices import vector, dot, cross, matrix2_inv, matrix_mult, matrix_mult_vector
 from PIL import Image
 import cmath
@@ -17,18 +20,21 @@ from datetime import datetime
 import uuid
 import sys
 import imageio
+from multiprocessing import Pool
+from tqdm import trange
+import numpy as np
 
 
 def angles_from_pixel_coords(point, x_size=1000):
     """map from pixel coords to (0, 2*pi) x (-pi/2, pi/2) rectangle"""
-    y_size = x_size / 2  #assume equirectangular format
+    y_size = x_size / 2  # assume equirectangular format
     return ((point[0] + 0.5) * 2 * pi / float(x_size),
             point[1] * pi / float(y_size - 1) - 0.5 * pi)
 
 
 def pixel_coords_from_angles(point, x_size=1000):
     """map from (0, 2*pi) x (-pi/2, pi/2) rectangle to pixel coords"""
-    y_size = x_size / 2  #assume equirectangular format
+    y_size = x_size / 2  # assume equirectangular format
     return (point[0] * float(x_size) / (2 * pi) - 0.5,
             (point[1] + 0.5 * pi) * float(y_size - 1) / pi)
 
@@ -94,22 +100,23 @@ def clamp(pt, x_size):
 def get_pixel_colour(pt, s_im, x_size=1000):
     """given pt in integers, get pixel colour on the source image as a vector in the colour cube"""
     pt = clamp(pt, x_size)
-    return vector(s_im[pt[0], pt[1]])
+    return vector(s_im[int(pt[0]), int(pt[1])])
 
 
 def get_interpolated_pixel_colour(pt, s_im, x_size=1000):
     """given pt in floats, linear interpolate pixel values nearby to get a good colour"""
-    ### for proper production software, more than just the four pixels nearest to the input point coordinates should be used in many cases
+    # for proper production software, more than just the four pixels nearest to the input point coordinates should be used in many cases
     x, y = int(floor(pt[0])), int(floor(
-        pt[1]))  #integer part of input coordinates
-    f, g = pt[0] - x, pt[1] - y  #fractional part of input coordinates
-    out_colour = (1-f)*( (1-g)*get_pixel_colour([x,y], s_im, x_size = x_size) + g*get_pixel_colour([x,y+1], s_im, x_size = x_size) ) \
-            +  f*( (1-g)*get_pixel_colour([x+1,y], s_im, x_size = x_size) + g*get_pixel_colour([x+1,y+1], s_im, x_size = x_size) )
+        pt[1]))  # integer part of input coordinates
+    f, g = pt[0] - x, pt[1] - y  # fractional part of input coordinates
+    out_colour = (1-f)*((1-g)*get_pixel_colour([x, y], s_im, x_size=x_size) + g*get_pixel_colour([x, y+1], s_im, x_size=x_size)) \
+        + f*((1-g)*get_pixel_colour([x+1, y], s_im, x_size=x_size) +
+             g*get_pixel_colour([x+1, y+1], s_im, x_size=x_size))
     out_colour = [int(round(coord)) for coord in out_colour]
     return tuple(out_colour)
 
 
-######## Functions generating SL(2,C) matrices
+# Functions generating SL(2,C) matrices
 
 # Note that we only care about the matrices projectively. I.e. [[a,b],[c,d]] acts in exactly the same way on points of CP^1 as
 # [[-a,-b],[-c,-d]], so we can also think of these matrices as being in PSL(2,C).
@@ -117,7 +124,7 @@ def get_interpolated_pixel_colour(pt, s_im, x_size=1000):
 
 def inf_zero_one_to_triple(p, q, r):
     """returns SL(2,C) matrix that sends the three points infinity, zero, one to given input points p,q,r"""
-    ### infinity = [1,0], zero = [0,1], one = [1,1] in CP^1
+    # infinity = [1,0], zero = [0,1], one = [1,1] in CP^1
     p1, p2 = p
     q1, q2 = q
     r1, r2 = r
@@ -152,8 +159,8 @@ def get_vector_perp_to_p_and_q(p, q):
     """p and q are distinct points on sphere, return a unit vector perpendicular to both"""
     if abs(
             dot(p, q) + 1
-    ) < 0.0001:  ### deal with the awkward special case when p and q are antipodal on the sphere
-        if abs(dot(p, vector([1, 0, 0]))) > 0.9999:  #p is parallel to (1,0,0)
+    ) < 0.0001:  # deal with the awkward special case when p and q are antipodal on the sphere
+        if abs(dot(p, vector([1, 0, 0]))) > 0.9999:  # p is parallel to (1,0,0)
             return vector([0, 1, 0])
         else:
             return cross(p, vector([1, 0, 0])).normalised()
@@ -185,11 +192,11 @@ def zoom_in_on_pixel_coords(p, zoom_factor, x_size=1000):
     # Note that the zoom factor is only accurate at the point p itself. As we move away from p, we zoom less and less.
     # We zoom with the inverse zoom_factor towards/away from the antipodal point to p.
     M_rot = rotate_pixel_coords_p_to_q(p, (0, 0), x_size=x_size)
-    M_scl = [[zoom_factor, 0], [0, 1]]  ### zoom in on zero in CP^1
+    M_scl = [[zoom_factor, 0], [0, 1]]  # zoom in on zero in CP^1
     return matrix_mult(matrix_mult(matrix2_inv(M_rot), M_scl), M_rot)
 
 
-##### Apply functions to images
+# Apply functions to images
 
 
 def apply_SL2C_elt_to_image(M,
@@ -252,6 +259,78 @@ def rotate_equirect_image(image_filename, from_x, to_x):
     return temp_file_name
 
 
+def generate_image_pt(i, j, in_x_size, out_x_size, M_rot, M_rot_inv, zoom_factor, zoom_loop_value, zoom_cutoff, s_im_A, s_im_B):
+    pt = (i, j)
+    pt = angles_from_pixel_coords(pt, x_size=out_x_size)
+    pt = sphere_from_angles(pt)
+    pt = CP1_from_sphere(pt)
+    pt = matrix_mult_vector(M_rot, pt)
+
+    # if ever you don't know how to do some operation in complex projective coordinates, it's almost certainly
+    # safe to just switch back to ordinary complex numbers by pt = pt[0]/pt[1]. The only danger is if pt[1] == 0,
+    # or is near enough to cause floating point errors. In this application, you are probably fine unless you
+    # make some very specific choices of where to zoom to etc.
+    pt = pt[0] / pt[1]
+    pt = cmath.log(pt)
+
+    # zoom_loop_value is between 0 and 1, vary this from 0.0 to 1.0 to animate frames zooming into the transition animation image
+    pt = complex(pt.real + log(zoom_factor) * zoom_loop_value, pt.imag)
+
+    recurse_value = (pt.real + zoom_cutoff) / log(zoom_factor)
+
+    # zoom_cutoff alters the slice of the input image that we use, so that it covers mostly the original image, together with
+    # some of the zoomed image that was composited with the original. The slice needs to cover the seam between the two
+    # (i.e. the picture frame you are using, but should cover as little as possible of the zoomed version of the image.
+
+    if (floor(recurse_value) >= 0.0):
+        # main and prev spheres => do nothing to pt
+        someval = "do nothing further"
+    elif (floor(recurse_value) == -1.0):
+        # this is the "next sphere"
+        pt = complex(
+            (pt.real + zoom_cutoff) % log(zoom_factor) - zoom_cutoff,
+            pt.imag)
+    elif (floor(recurse_value) == -2.0):
+        pt = complex((pt.real + zoom_cutoff) % log(zoom_factor) -
+                     zoom_cutoff - log(zoom_factor), pt.imag)
+    elif (floor(recurse_value) == -3.0):
+        pt = complex((pt.real + zoom_cutoff) % log(zoom_factor) -
+                     zoom_cutoff - (log(zoom_factor) * 2), pt.imag)
+    else:
+        # this is "future spheres"
+        pt = complex((pt.real + zoom_cutoff) % log(zoom_factor) -
+                     zoom_cutoff - (log(zoom_factor) * 3.0), pt.imag)
+
+    pt = cmath.exp(pt)
+    pt = [pt, 1]  # back to projective coordinates
+    pt = matrix_mult_vector(M_rot_inv, pt)
+    pt = sphere_from_CP1(pt)
+    pt = angles_from_sphere(pt)
+    pt = pixel_coords_from_angles(pt, x_size=in_x_size)
+
+    if (floor(recurse_value) >= 0):
+        return get_interpolated_pixel_colour(
+            pt, s_im_A, in_x_size)
+    else:
+        return get_interpolated_pixel_colour(
+            pt, s_im_B, in_x_size)
+
+
+def get_color(in_x_size, out_x_size, out_y_size, M_rot, M_rot_inv, zoom_factor, zoom_loop_value, zoom_cutoff, s_im_A, s_im_B):
+    out_color = list()
+
+    for _ in trange(1):
+        args = list()
+        for i in range(out_x_size):
+            for j in range(out_y_size):
+                args.append((i, j, in_x_size, out_x_size, M_rot, M_rot_inv, zoom_factor,
+                            zoom_loop_value, zoom_cutoff, s_im_A, s_im_B))
+
+        with Pool(processes=4) as pool:
+            out_color = pool.starmap(generate_image_pt, args)
+    return out_color
+
+
 def generate_image(zoom_center_pixel_coords,
                    zoom_factor,
                    zoom_cutoff,
@@ -262,99 +341,57 @@ def generate_image(zoom_center_pixel_coords,
                    save_filename="sphere_transforms_test.png"):
     """produces a zooming effect image from one equirectangular image into another equirectangular image"""
     source_image_A = Image.open(source_image_filename_A)
-    s_im_A = source_image_A.load()
-    source_image_B = Image.open(source_image_filename_B)
-    s_im_B = source_image_B.load()
-
     in_x_size, in_y_size = source_image_A.size
+    loaded_A = source_image_A.load()
+    s_im_A = np.zeros((in_x_size, in_y_size), dtype=tuple)
+    for i in range(in_x_size):
+        for j in range(in_y_size):
+            s_im_A[i, j] = loaded_A[i, j]
+
+    source_image_B = Image.open(source_image_filename_B)
+    loaded_B = source_image_B.load()
+    s_im_B = np.zeros((in_x_size, in_y_size), dtype=tuple)
+    for i in range(in_x_size):
+        for j in range(in_y_size):
+            s_im_B[i, j] = loaded_B[i, j]
 
     M_rot = rotate_pixel_coords_p_to_q(zoom_center_pixel_coords, (0, 0),
                                        x_size=in_x_size)
     M_rot_inv = matrix2_inv(M_rot)
     out_y_size = int(out_x_size / 2)
+
+    out_color = get_color(in_x_size, out_x_size, out_y_size, M_rot, M_rot_inv, zoom_factor,
+                          zoom_loop_value, zoom_cutoff, s_im_A, s_im_B)
+
     out_image = Image.new("RGB", (out_x_size, out_y_size))
     o_im = out_image.load()
-
     for i in range(out_x_size):
         for j in range(out_y_size):
-            pt = (i, j)
-            pt = angles_from_pixel_coords(pt, x_size=out_x_size)
-            pt = sphere_from_angles(pt)
-            pt = CP1_from_sphere(pt)
-            pt = matrix_mult_vector(M_rot, pt)
-
-            # if ever you don't know how to do some operation in complex projective coordinates, it's almost certainly
-            # safe to just switch back to ordinary complex numbers by pt = pt[0]/pt[1]. The only danger is if pt[1] == 0,
-            # or is near enough to cause floating point errors. In this application, you are probably fine unless you
-            # make some very specific choices of where to zoom to etc.
-            pt = pt[0] / pt[1]
-            pt = cmath.log(pt)
-
-            # zoom_loop_value is between 0 and 1, vary this from 0.0 to 1.0 to animate frames zooming into the transition animation image
-            pt = complex(pt.real + log(zoom_factor) * zoom_loop_value, pt.imag)
-
-            recurse_value = (pt.real + zoom_cutoff) / log(zoom_factor)
-
-            # zoom_cutoff alters the slice of the input image that we use, so that it covers mostly the original image, together with
-            # some of the zoomed image that was composited with the original. The slice needs to cover the seam between the two
-            # (i.e. the picture frame you are using, but should cover as little as possible of the zoomed version of the image.
-
-            if (floor(recurse_value) >= 0.0):
-                # main and prev spheres => do nothing to pt
-                someval = "do nothing further"
-            elif (floor(recurse_value) == -1.0):
-                # this is the "next sphere"
-                pt = complex(
-                    (pt.real + zoom_cutoff) % log(zoom_factor) - zoom_cutoff,
-                    pt.imag)
-            elif (floor(recurse_value) == -2.0):
-                pt = complex((pt.real + zoom_cutoff) % log(zoom_factor) -
-                             zoom_cutoff - log(zoom_factor), pt.imag)
-            elif (floor(recurse_value) == -3.0):
-                pt = complex((pt.real + zoom_cutoff) % log(zoom_factor) -
-                             zoom_cutoff - (log(zoom_factor) * 2), pt.imag)
-            else:
-                # this is "future spheres"
-                pt = complex((pt.real + zoom_cutoff) % log(zoom_factor) -
-                             zoom_cutoff - (log(zoom_factor) * 3.0), pt.imag)
-
-            pt = cmath.exp(pt)
-            pt = [pt, 1]  #back to projective coordinates
-            pt = matrix_mult_vector(M_rot_inv, pt)
-            pt = sphere_from_CP1(pt)
-            pt = angles_from_sphere(pt)
-            pt = pixel_coords_from_angles(pt, x_size=in_x_size)
-
-            if (floor(recurse_value) >= 0):
-                o_im[i,
-                     j] = get_interpolated_pixel_colour(pt, s_im_A, in_x_size)
-            else:
-                o_im[i,
-                     j] = get_interpolated_pixel_colour(pt, s_im_B, in_x_size)
+            o_im[i, j] = out_color[i * out_y_size + j]
 
     sys.stdout.write(" . " + datetime.now().strftime('%H:%M:%S'))
     # print datetime.now().strftime('%H:%M:%S') + ": finished " + save_filename
     out_image.save(save_filename)
 
 
-######## Test
+# Test
 
 if __name__ == "__main__":
-    #M = zoom_in_on_pixel_coords((360,179.5), 2, x_size = 720)
-    #apply_SL2C_elt_to_image( M, 'equirectangular_test_image.png', save_filename = 'scaled_test_image.png' )
+    # M = zoom_in_on_pixel_coords((360,179.5), 2, x_size = 720)
+    # apply_SL2C_elt_to_image( M, 'equirectangular_test_image.png', save_filename = 'scaled_test_image.png' )
 
     tmp_file_name = rotate_equirect_image(
         '.\\matterport_pano_images\\ee59d6b5e5da4def9fe85a8ba94ecf25_skybox_pano.jpg',
         783, 1842)
 
-    TOTAL = 4
-    for i in range(TOTAL + 1):
+    TOTAL = 1
+    for i in range(TOTAL):
         generate_image(
             (2048 - 1632, 604), 4, 1.2,
             '.\\matterport_pano_images\\00ebbf3782c64d74aaf7dd39cd561175_skybox_pano.jpg',
             tmp_file_name, 2048, i / TOTAL, "{}.png".format(i))
 
-    imgs = []
-    for i in range(TOTAL):
-        imgs.append(imageio.imread("{}.png".format(i)))
-    imageio.mimsave('test.mp4', imgs)
+    # imgs = []
+    # for i in range(TOTAL):
+    #     imgs.append(imageio.imread("{}.png".format(i)))
+    # imageio.mimsave('test.mp4', imgs)
